8457d32b72eea906d29c1d3ff17b9f865bdaaa1a
JEE <jeerawut.jd40@gmail.com>
2024-03-18 12:32:27 +0700

    handle logic save pdf and download file

--- export/views.py
+++ export/views.py
@@ -1,33 +1,22 @@
-import os, json, uuid
+import os
+from io import BytesIO
+from django.http import FileResponse
 from pyreportjasper import PyReportJasper
 from django.shortcuts import render
-from django.conf import settings
 
 
-def json_to_pdf():
+def json_to_pdf(title="FooKCompany"):
     RESOURCES_DIR = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'resources')
     REPORTS_DIR = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'reports')
-    input_file = os.path.join(REPORTS_DIR, 'Invoice.jrxml')
-    output_file = os.path.join(RESOURCES_DIR, 'Invoice.pdf')
-
-    generate_utils_dir = os.path.join(settings.BASE_DIR, 'export', 'generate')
-    if not os.path.exists(generate_utils_dir):
-        os.mkdir(generate_utils_dir)
-
-    parameters = {"title": "TestTitle", "sub_title": "TestSubTitle"}
-    json_obj = json.dumps({"data": {}}, indent=4, ensure_ascii=False)
-    json_filename = str(uuid.uuid4().hex) + '.json'
-    json_file = os.path.join(generate_utils_dir, json_filename)
-    with open(json_file, "w") as outfile:
-        outfile.write(json_obj)
-
+    input_file = os.path.join(REPORTS_DIR, 'Blank_A4.jrxml')
+    output_file = os.path.join(RESOURCES_DIR, 'Blank_A4.pdf')
+    json_filename = 'data.json'
+    json_file = os.path.join(REPORTS_DIR, json_filename)
+    parameters = {"Title": title}
     conn = {
         'driver': 'json',
-        'data_file': json_file,
+        'data_file': json_file
     }
-
-    if output_file is None:
-        output_file = os.path.join(generate_utils_dir, str(uuid.uuid4().hex))
 
     pyreportjasper = PyReportJasper()
     pyreportjasper.config(
@@ -38,9 +27,18 @@ def json_to_pdf():
         db_connection=conn
     )
     pyreportjasper.process_report()
+    return output_file
+
+def pdf_to_buffer(pdf_file_path):
+    with open(pdf_file_path, "rb") as f:
+        return BytesIO(f.read())
 
 def index(request):
-    rs = json_to_pdf()
-    print(f"rs: {rs}")
     context = {}
+    if request.method == 'POST':
+        data = request.POST.copy()
+        title = data.get("title", "Ex.Topic")
+        buffer = pdf_to_buffer(json_to_pdf(title))
+        return FileResponse(buffer, as_attachment=True, filename='helloworld-jasper.pdf')
+
     return render(request, 'index.html', context)
