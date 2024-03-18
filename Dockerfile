5b69ca3733b4c1e8781b82c28774aef516179a14
JEE <jeerawut.jd40@gmail.com>
2024-03-18 15:29:23 +0700

    update config build

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,18 @@
 FROM python:3.9.16
 
+WORKDIR /app
+
+ENV PYTHONUNBUFFERED 1
+ENV PYTHONDONTWRITEBYTECODE 1
+
 RUN apt-get update
 RUN apt-get install openjdk-11-jdk -y
 ENV JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64/
-RUN pip install --upgrade pip
 
-WORKDIR /code
-COPY . /code
+RUN pip install --upgrade pip
+COPY ./requirements.txt /app/
 RUN pip install -r requirements.txt
+
+COPY . /app
+
+ENTRYPOINT [ "gunicorn", "core.wsgi", "-b", "0.0.0.0:8000"]
--- mysite/settings.py
+++ mysite/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -121,8 +122,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
-MEDIA_URL = 'media/'
+STATIC_URL = BASE_DIR + 'static/'
+MEDIA_URL = BASE_DIR + 'media/'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
