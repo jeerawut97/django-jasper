e806e8efe7fa38b75582ac6a79ca6c5ac481de24
JEE <jeerawut.jd40@gmail.com>
2024-03-18 16:37:14 +0700

    Change base dir

--- mysite/settings.py
+++ mysite/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = BASE_DIR + 'static/'
+STATIC_URL = os.path.join(BASE_DIR, 'static/')
 STATIC_ROOT = 'staticfiles'
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
